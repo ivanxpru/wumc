086b1449c04d73573ced641b75b70646a797f6ae
ivanxpru (lenovo) <8034400+ivanxpru@users.noreply.github.com>
2020-01-29 20:43:36 +0500

    test src

--- src/app/routes/api.js
+++ src/app/routes/api.js
@@ -2,8 +2,9 @@ const express = require('express');
 const fs = require('fs');
 
 const router = express.Router();
+let movies = {};
 const library = require('../modules/library');
-const movies = require('../../data/movies.json');
+// const movies = require('../../data/movies.json');
 const serials = require('../../data/serials.json');
 
 const sortTitles = (a, b) => {
@@ -51,6 +52,7 @@ router.get('/movies', (_req, res) => {
   if (!fs.existsSync('../../data/movies.json')) {
     library.getMovies();
   }
+  movies = require('../../data/movies.json');
   movies.titles.forEach((movie) => {
     titles.push(movie);
   });
