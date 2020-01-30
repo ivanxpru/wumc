e6afc2d94ac97f9e38af8f33a90753e5d8cfaad9
ivanxpru (lenovo) <8034400+ivanxpru@users.noreply.github.com>
2020-01-30 09:47:07 +0500

    babel es2015

--- webpack.config.js
+++ webpack.config.js
@@ -42,7 +42,7 @@ module.exports = {
           loader: 'babel-loader',
           options: {
             presets: [
-              ['es2015-script',
+              ['es2015',
                 { modules: false }],
             ],
           },
